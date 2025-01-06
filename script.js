61dbab714b25d8db2faeaabe6709ae084414cfa2
Caldwell10 <caldwell.kibe@strathmore.edu>
2025-01-06 21:42:03 +0300

    Finalized portfolio

--- script.js
+++ script.js
@@ -5,16 +5,12 @@ const projectBoxes = document.querySelectorAll('.box');
 // Add event listener to each filter button
 filterButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        // Remove 'active' class from all buttons
+       
         filterButtons.forEach((btn) => btn.classList.remove('active'));
-
-        // Add 'active' class to the clicked button
         button.classList.add('active');
-
-        // Get the category to filter
         const category = button.getAttribute('data-category');
 
-        // Show/hide projects based on the category
+       
         projectBoxes.forEach((box) => {
             if (category === 'all' || box.getAttribute('data-category').includes(category)) {
                 box.style.display = 'block';
@@ -24,3 +20,12 @@ filterButtons.forEach((button) => {
         });
     });
 });
+
+const menu = document.querySelector("#menu")
+const nav = document.querySelector(".links")
+
+menu.onclick =() => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+
+}
